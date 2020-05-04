7bf879d2c9ee750e25dc7c45cdd1fc54246dbb55
Brandon Berhent <bbedward@gmail.com>
2020-05-04 14:13:46 -0400

    Revert nanopy change

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def requirements() -> list:
             'aioredlock>=0.3.0',
             'python-dotenv>=0.10.3',
             'python-rapidjson>=0.9.1',
-            'nanopy @ git+https://github.com/bbedward/nanopy#egg=nanopy',
+            'nanopy>=20.0.0',
             'aiohttp>=3.6.2',
             'pyyaml',
             'pycryptodome>=3.9.4',

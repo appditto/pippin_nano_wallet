2f9764316cad68fad93b0f785bb3c45121ee0464
Brandon Berhent <bbedward@gmail.com>
2024-07-01 19:40:37 -0400

    Allow numerical values in send request

--- apps/server/models/requests/send_request.go
+++ apps/server/models/requests/send_request.go
@@ -1,10 +1,46 @@
 package requests
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type SendRequest struct {
 	BaseRequest `mapstructure:",squash"`
-	Source      string  `json:"source" mapstructure:"source"`
-	Destination string  `json:"destination" mapstructure:"destination"`
-	Amount      string  `json:"amount" mapstructure:"amount"`
-	ID          *string `json:"id,omitempty" mapstructure:"id,omitempty"`
-	Work        *string `json:"work,omitempty" mapstructure:"work,omitempty"`
+	Source      string          `json:"source" mapstructure:"source"`
+	Destination string          `json:"destination" mapstructure:"destination"`
+	Amount      string          `json:"-"` // Exclude from automatic unmarshalling
+	RawAmount   json.RawMessage `json:"amount" mapstructure:"amount"`
+	ID          *string         `json:"id,omitempty" mapstructure:"id,omitempty"`
+	Work        *string         `json:"work,omitempty" mapstructure:"work,omitempty"`
+}
+
+func (r *SendRequest) UnmarshalJSON(data []byte) error {
+	type Alias SendRequest
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(r),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	var temp interface{}
+	if err := json.Unmarshal(r.RawAmount, &temp); err != nil {
+		return err
+	}
+
+	switch v := temp.(type) {
+	case float64:
+		r.Amount = fmt.Sprintf("%.0f", v) // Safely convert to string without scientific notation
+	case int:
+		r.Amount = fmt.Sprintf("%d", v)
+	case string:
+		r.Amount = v
+	default:
+		return fmt.Errorf("amount is of a type I don't know how to handle")
+	}
+
+	return nil
 }
--- apps/server/models/requests/send_request_test.go
+++ apps/server/models/requests/send_request_test.go
@@ -21,6 +21,19 @@ func TestDecodeSendRequest(t *testing.T) {
 	assert.Nil(t, decoded.Work)
 }
 
+func TestDecodeSendRequestNumericAmount(t *testing.T) {
+	encoded := `{"action":"send","wallet":"1234","source":"nano_1","destination":"nano_2","bpow_key":"abc","amount":1234}`
+	var decoded SendRequest
+	json.Unmarshal([]byte(encoded), &decoded)
+	assert.Equal(t, "send", decoded.Action)
+	assert.Equal(t, "1234", decoded.Wallet)
+	assert.Equal(t, "nano_1", decoded.Source)
+	assert.Equal(t, "nano_2", decoded.Destination)
+	assert.Equal(t, "abc", *decoded.BpowKey)
+	assert.Equal(t, "1234", decoded.Amount)
+	assert.Nil(t, decoded.Work)
+}
+
 func TestMapStructureDecodeSendRequest(t *testing.T) {
 	request := map[string]interface{}{
 		"action":      "send",
